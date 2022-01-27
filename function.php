c5777ec8c7f7e0465e1d6d2b60ef6240e65132d3
guillaume perroy <gui.perroy@gmail.com>
2022-01-27 13:50:45 +0100

    séparation des codes en plusieurs fichier

--- function.php
+++ function.php
@@ -4,4 +4,20 @@ function tva(?float $price): ?float{
     $price = round($price /1.2, 2, PHP_ROUND_HALF_UP);
     return  $price;
 }
+
+
+
+function affichage($product){
+    ?>
+         <tr>
+             <td><?= $product['name'];?></td>
+             <td><?= tva($product['price taxed']);?></td>
+             <td <?php if ($product['price taxed'] <= 12) { ?>style ="color:green"<?php } ?>>
+             <?= $product['price taxed']; ?></td>
+                 
+             <td> <?= $product['description']; ?></td>
+         </tr>
+<?php
+}
 ?>
+
--- premier.php
+++ premier.php
@@ -1,30 +1,6 @@
 <?php
 include 'function.php';
-
-$description= "ces produits de grande qualité conféctionnés a la main par Manon dans le respect de l'environnement et des différents acteurs ";
-
-$tableau = [
-    1=>['name' => 'Bonnet en laine',
-
-    'price taxed' =>10,
-'description' => $description],
-
-2=>['name' =>'Bonnet en laine bio',
-
-'price taxed' =>14,
-'description' => $description],
-
-3=>['name' =>'Bonnet en laine et cachemire',
-
-'price taxed' =>20,
-'description' => $description],
-
-4=>['name' =>'Bonnet arc-en-ciel',
-
-'price taxed' =>18,
-'description' => $description]
-];
-
+include 'variables.php';
 ?>
 
 
@@ -50,17 +26,10 @@ $tableau = [
           </tr>
         <?php 
         foreach ($tableau as $key => $product) {?>
-         <tr>
-             <td><?php echo $product['name'];?></td>
-             <td><?php echo tva($product['price taxed']);?></td>
-             <td <?php if ($product['price taxed']< 16) {?>style ="color:green"<?php } ?>>
-             <?= $product['price taxed'] ;?></td>
-                 
-             <td> <?php echo $product['description']?></td>
-         </tr>
+         <?php echo affichage($product);?>
       <?php } ?>
      </table>
+
  </body>
  </html>
 
- 
